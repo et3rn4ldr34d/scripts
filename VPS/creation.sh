0e7c88d196e3e1dab9513b0dd84612f2b10b20c9
viol3nthacks <vega.sonke@gmail.com>
2022-10-08 13:53:20 -0700

    Cleaned up code

--- VPS/creation.sh
+++ VPS/creation.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 
 # Infrastructure
-ID='118213687'
 VMNAME='Ein'
+ID='118213687'
 IP='146.190.14.41'
 
 # Colors
+WHITE='\033[1;37m'
 GREEN='\033[1;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 # 1. Create DO Droplet from Snapshot | Wait until created to proceed
-echo -e "[${RED}o${NC}] ${GREEN}Creating Droplet: ${NC} ${WHITE}${VMNAME}${NC}...";
+echo -e "[${RED}o${NC}] ${GREEN}Creating Droplet: ${WHITE}${VMNAME}${NC}";
 doctl compute droplet create ${VMNAME} --image ${ID} --size s-2vcpu-2gb --region sfo3 --wait;
 
-#2. Find Droplet Information | Filter | Pass into Variable
+# 2. Find Droplet Information | Filter | Pass into Variable
 echo -e "[${RED}o${NC}] ${GREEN}Finding ID${NC}...";
-VMID=$(doctl compute droplet get Ein --format ID | tr -d "ID");
+VMID=$(doctl compute droplet get ${VMNAME} --format ID | tr -d "ID");
 
 # 3. Assign Reserved IP to Droplet
-echo -e "[${RED}o${NC}] ${GREEN}Logging in${NC}...";
 doctl compute reserved-ip-action assign ${IP} ${VMID} &&
-sleep 10;
+echo -e "[${RED}o${NC}] ${GREEN}IP Assigned${NC}...";
 
 # 4. Login to Droplet into SSH
-ssh root@${IP}; # test
+
+# Pings IP | If available, connect, if not, wait
+sleep 30;
+ping -c 1 ${IP};
+if echo $?=0
+then
+    echo -e "[${RED}o${NC}] ${GREEN}Logging in${NC}...";
+    ssh root@${IP}; # test
+else
+    echo -e "[${RED}o${NC}] ${RED}Unavailable${NC}.";
+fi
 # ssh -L 59000:localhost:5901 -C -N -l root ${IP};
-echo -e "[${GREEN}o${NC}] ${GREEN}Connected${NC}."
+# echo -e "[${GREEN}o${NC}] ${GREEN}Connected${NC}."
--- VPS/destruction.sh
+++ VPS/destruction.sh
@@ -3,7 +3,7 @@
 # Infrastructure
 ID='118213687'
 NAME='Ein'
-IP=''
+IP='146.190.14.41'
 
 # Colors
 WHITE='\033[1;37m'
@@ -14,17 +14,31 @@ NC='\033[0m' # No Color
 # echo -e "[${RED}o${NC}] ${GREEN}Copying Files..."${NC};
 
 # Steps:
-# 1. Export files to local storage
+# 1. Zips Files | Export files to local storage
 
-# scp username@b:/path/to/file /path/to/destination;
-# echo -e "[${RED}o${NC}] ${GREEN}Creating Snapshot..."${NC};
+# tar -czvf name-of-archive.tar.gz /path/to/directory-or-file
+# -c: Create an archive.
+# -z: Compress the archive with gzip.
+# -v: Display progress in the terminal while creating the archive, also known as “verbose” mode. The v is always optional in these commands, but it’s helpful.
+# -f: Allows you to specify the filename of the archive.
 
-# 2. Create Snapshot | Wait for Completion before next command
+# (Example File  /users/admin/downloads/* root@155.138.150.12:/home/)
+# scp root@${IP}:/path/to/file /path/to/destination &&
+# echo -e "[${RED}o${NC}] ${GREEN}Creating New Snapshot..."${NC};
+
+# 2. Create Snapshot | Wait for Completion before next command | Deletes Old Snapshot
 
 # doctl compute droplet-action snapshot ${ID} --wait;
-echo -e "[${RED}o${NC}] ${GREEN}Destroying Droplet:${NC} ${WHITE}${NAME} ..."${NC};
+echo -e "[${RED}o${NC}] ${GREEN}Destroying Old Snapshot:${NC} ${WHITE}${NAME}${NC}";
+
+echo -e "[${RED}o${NC}] ${GREEN}Destroying Droplet:${NC} ${WHITE}${NAME}${NC}";
 
 # 3. Destroys Droplet
 
 doctl compute droplet delete ${NAME} -f; # -force
-echo -e "[${GREEN}o${NC}] ${GREEN}Complete."${NC}
+echo -e "[${GREEN}|${NC}] ${GREEN}Completed${NC}.";
+echo -e "[${RED}!${NC}] ${GREEN}Current Balance${NC}:";
+
+# 4. Displays Current Bill
+
+doctl balance get

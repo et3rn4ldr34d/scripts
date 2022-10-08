31872aa29b03a0c73634116c252414a195f1afab
viol3nthacks <vega.sonke@gmail.com>
2022-10-07 19:59:27 -0700

    Added Static IP

--- VPS/creation.sh
+++ VPS/creation.sh
@@ -2,7 +2,7 @@
 
 # Infrastructure
 ID='118213687'
-VMID='320000562'
+VMID=''
 VMNAME='Ein'
 IP='146.190.14.41'
 
@@ -11,24 +11,25 @@ GREEN='\033[1;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-echo -e "[${RED}-${NC}] ${GREEN}Creating Droplet: ${NC} ${WHITE}${VMNAME}..."${NC};
+echo -e "[${RED}o${NC}] ${GREEN}Creating Droplet: ${NC} ${WHITE}${VMNAME}${NC}...";
 
 # Steps:
-# 1. Create DO Droplet from Snapshot
+# 1. Create DO Droplet from Snapshot | Wait until created to proceed
 
 doctl compute droplet create ${VMNAME} --image ${ID} --size s-2vcpu-2gb --region sfo3 --wait;
-# Creates Droplet | Waits for Creation before next command
-echo -e "[${RED}-${NC}] ${GREEN}Logging in..."${NC};
+echo -e "[${RED}o${NC}] ${GREEN}Finding ID${NC}...";
 
-# 2. Assign Reserved IP to Droplet
+#2. Find Droplet Information | Filter | Pass into Variable
+
+doctl compute droplet get Ein --format ID | tr -d "ID" | $VMID;
+
+# 3. Assign Reserved IP to Droplet
 
 doctl compute reserved-ip-action assign ${IP} ${VMID};
-# Assigns Static IP
+echo -e "[${RED}o${NC}] ${GREEN}Logging in${NC}...";
 
 # 3. Login to Droplet into SSH
 
+# ssh root@${IP}; # test
 # ssh -L 59000:localhost:5901 -C -N -l root ${IP};
-ssh root@${IP};
-
-# Creates SSH connection
 echo -e "[${GREEN}o${NC}] ${GREEN}Connected."${NC}
